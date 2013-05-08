a0212b6e3bc8746242bd62b4bdbc2f0ff5c8cd2e
Doris Ip <dorisip@gmail.com>
2013-05-07 20:52:39 -0400

    Support only file renames in landing zones

--- windy_tales/watcher/event_handler.py
+++ windy_tales/watcher/event_handler.py
@@ -8,25 +8,27 @@ from windy_tales.flat_file.parser import flat_file_to_json
 from windy_tales.database.connection import WindyDbConnection
 from windy_tales.database.collections.genericCollection import GenericCollection
 from windy_tales.utils.archive import achive_file
+import os
 
 
 class WatcherEventHandler(FileSystemEventHandler):
 
-    def on_created(self, event):
+    def on_moved(self, event):
         '''
-        Listen to file/directory creation events
-        Ignoring file modification and file renames
+        Only listen for move or rename from landing zone to a file within the landing zone
         '''
         if event.is_directory is False:
-            print("New file created in LZ: " + event.src_path)
+            file_name, file_ext = os.path.splitext(event.dest_path)
+            if file_ext == '.flat':
+                print("File created renamed in LZ: " + event.dest_path)
 
-            # New file is received, convert flat file to json format
-            json_format = flat_file_to_json(event.src_path)
+                # New file is received, convert flat file to json format
+                json_format = flat_file_to_json(event.dest_path)
 
-            # archive the file
-            achive_file(event.src_path)
+                # archive the file
+                achive_file(event.dest_path)
 
-            with WindyDbConnection() as connection:
-                genericCollection = GenericCollection(connection, 'dummyname')
-                genericCollection.save(json_format)
-            print(json_format)
+                with WindyDbConnection() as connection:
+                    genericCollection = GenericCollection(connection, 'dummyname')
+                    genericCollection.save(json_format)
+                print(json_format)
