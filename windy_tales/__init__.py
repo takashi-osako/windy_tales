26097d5b161a5eb5b938dcf26e056cc51dfef137
Takashi Osako <tosako@wgen.net>
2013-05-11 22:50:56 -0400

    load data when windy starts up

--- windy_tales/__init__.py
+++ windy_tales/__init__.py
@@ -14,6 +14,8 @@ from cloudy_tales.database.client import create_db_client
 from windy_tales.database.connection import WindyDbConnection
 from windy_tales.database.collections.header_file_parsed_template import HeaderfileParsedTemplate
 from cloudy_tales.data_fusion.translate import generate_templated_json
+from windy_tales.utils.data_loader import load_data_from_flatfile
+from windy_tales.database.collections.genericCollection import GenericCollection
 
 watcher = Watcher('/tmp/lz')
 
@@ -25,7 +27,8 @@ def main():
     # initialize mongodb
     create_db_client()
     load_template()
-    load_data()
+    clear_data()
+    load_flatfile_data()
 
     watcher.watch_dir()
     signal.signal(signal.SIGTERM, signal_handler)
@@ -58,8 +61,20 @@ def load_template():
             headerFileParsedTemplate.save(data_name=json.keys()[0], data=json, version=1)
 
 
-def load_data():
-    pass
+def clear_data():
+    targets = ['Supplier']
+    with WindyDbConnection() as connection:
+        for target in targets:
+            colleciton = GenericCollection(connection=connection, name=target)
+            colleciton.remove()
+
+def load_flatfile_data():
+    flatfiles = ['flatfiles/supplier/supplier.flat']
+    here = os.path.abspath(os.path.dirname(__file__))
+    for flatfile in flatfiles:
+        file_name = os.path.join(here, 'resources', flatfile)
+        print(file_name)
+        load_data_from_flatfile(file_name)
 
 
 if __name__ == '__main__':
