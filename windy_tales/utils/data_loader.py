f6977e737922919860020cd7f0bb18d2bfa72f43
Takashi Osako <tosako@wgen.net>
2013-05-12 22:56:50 -0400

    create PDF when a transaction comes in

--- windy_tales/utils/data_loader.py
+++ windy_tales/utils/data_loader.py
@@ -7,9 +7,10 @@ from windy_tales.utils.utils import read_file
 from windy_tales.flat_file.parser import flat_to_json
 from windy_tales.database.connection import WindyDbConnection
 from windy_tales.database.collections.generic_collection import GenericCollection
-from windy_tales.data_fusion import template_json
 from windy_tales.data_aggregator.transaction_aggregator import aggregate_for_transaction
 import json
+from cloudy_tales.data_fusion.translate import combine_template_with_data
+from cloudy_tales.utils.getTemplate import get_template
 
 
 def load_data_from_flatfile(filename):
@@ -32,12 +33,13 @@ def load_data_from_flatfile(filename):
                 genericCollection = GenericCollection(connection, data_name)
             doc_id = genericCollection.save(json_format)
 
-            # TODO: TEMP:  template json from flat file data
-            template_json(doc_id['_id'], data_name, None)
-
         # if data is transheader, then aggregate data for Data Fusion Service
         if data_name == "Transheader":
             json_format = aggregate_for_transaction(json_format)
+            # TODO: TEMP:  template json from flat file data
+            template=get_template('test')
+            combine_template_with_data(template=template, data=json_format, write_to_file=True)
+
         print("#####")
         print(json.dumps(json_format))
         print("#####")
